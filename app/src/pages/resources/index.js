47f32d1251c2c0253bb4d6147e452f425b2d5500
Mark Johnson <markjohnson4542@gmail.com>
2018-03-14 15:36:51 -0400

    grid

--- app/src/pages/resources/index.js
+++ app/src/pages/resources/index.js
@@ -1,39 +1,54 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { map, compose, split, head } from 'ramda'
-import List, {
-  ListItem,
-  ListItemAvatar,
-  ListItemIcon,
-  ListItemSecondaryAction,
-  ListItemText
-} from 'material-ui/List'
+import { map } from 'ramda'
+import List, { ListItem, ListItemAvatar, ListItemText } from 'material-ui/List'
 import Avatar from 'material-ui/Avatar'
-import FolderIcon from 'material-ui-icons/Folder'
-const Resources = props => {
-  console.log('props', props)
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList'
+import Subheader from 'material-ui/List/ListSubheader'
+import IconButton from 'material-ui/IconButton'
+import InfoIcon from 'material-ui-icons/Info'
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 500,
+    height: 450
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)'
+  }
+})
+
+const Resources = props => {
   return (
     <div>
-      <h1>Resources</h1>
-      <div>
-        <ol>
-          {map(r => {
-            return (
-              <div>
-                <List>
-                  <ListItem>
-                    <ListItemAvatar>
-                      <Avatar>{r.name.charAt(0)}</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary={r.name} secondary={r.shortDesc} />
-                  </ListItem>
-                </List>
-              </div>
-            )
-          }, props.resources)}
-        </ol>
-      </div>
+      <GridList cellHeight={180}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <Subheader component="div">December</Subheader>
+        </GridListTile>
+        {props.resources.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src="https://3.bp.blogspot.com/-gnw1Xsq-5iI/V2m8yXy3DUI/AAAAAAAAK-g/H-K0DGNC43su0QAhSgAvNqMRbUEzPJ2egCLcB/s1600/Small-Soldiers-Archer-lifesize-figure-3.jpg" />
+            <GridListTileBar
+              title={tile.name}
+              subtitle={<span>{tile.shortDesc}</span>}
+              actionIcon={
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   )
 }
