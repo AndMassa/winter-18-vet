6a9351ca9d98bcdb89ce2446ce55c7430876c168
Mark Johnson <markjohnson4542@gmail.com>
2018-03-14 11:27:11 -0400

    loadData script to DB

--- api/lib/dal-helper.js
+++ api/lib/dal-helper.js
@@ -4,8 +4,6 @@ PouchDB.plugin(require('pouchdb-find'))
 const HTTPError = require('node-http-error')
 const { pluck } = require('ramda')
 
-console.log(`The db is ${process.env.COUCH_URL}${process.env.COUCH_DBNAME}`)
-
 const db = new PouchDB(`${process.env.COUCH_URL}${process.env.COUCH_DBNAME}`)
 
 const allDocs = options => {
